d278d7c60cbd4e6e9a8e4424f36be5f4a537937f
enzotsilva7-hub <enzo.t.silva7@aluno.senai.br>
2026-05-04 20:37:14 -0400

    Exercicio 2

---
+++ Over/Produto.java
@@ -0,0 +1,31 @@
+package Over;
+
+public class Produto {
+
+    String nome;
+    double preco;
+
+    Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{nome='" + nome + "', preco=" + preco + "}";
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto("Notebook da Apple", 100000.00);
+        Produto p2 = new Produto("iPhone 7", 18000.00);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        // achou caro pai? reclama com o Lula
+    }
+}
